162d669ad9f6dfbd2515c54b1c638ebb671788df
qinscode <fudong.qin@outlook.com>
2024-08-03 19:13:58 +0800

    refactor: refactor settings service to use TypeORM repository

--- backend/src/settings/settings.service.ts
+++ backend/src/settings/settings.service.ts
@@ -1,22 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Setting } from './entities/setting.entity';
 import { UpdateSettingsDto } from './dto/settings.dto';
 
 @Injectable()
 export class SettingsService {
-  private settings = {
-    universityName: 'Example University',
-    academicYear: '2023-2024',
-    pdfTemplate: 'template1',
-    handbookFormat: 'pdf',
-    defaultUserRole: 'editor',
-  };
+  constructor(
+    @InjectRepository(Setting)
+    private settingsRepository: Repository<Setting>,
+  ) {}
 
-  getSettings() {
-    return this.settings;
+  async getSettings(): Promise<Setting> {
+    let settings = await this.settingsRepository.findOne({ where: { id: 1 } });
+    if (!settings) {
+      settings = await this.settingsRepository.save({
+        universityName: 'Example University',
+        academicYear: '2023-2024',
+        pdfTemplate: 'template1',
+        handbookFormat: 'pdf',
+        defaultUserRole: 'editor',
+      });
+    }
+    return settings;
   }
 
-  updateSettings(updateSettingsDto: UpdateSettingsDto) {
-    this.settings = { ...this.settings, ...updateSettingsDto };
-    return this.settings;
+  async updateSettings(updateSettingsDto: UpdateSettingsDto): Promise<Setting> {
+    await this.settingsRepository.update(1, updateSettingsDto);
+    return this.getSettings();
   }
 }
