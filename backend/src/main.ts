adad51a1f42e4e1904a10fbd6573f1e2185a579f
qinscode <fudong.qin@outlook.com>
2024-08-03 19:06:04 +0800

    Test: Add global prefix and update rules controller and service tests

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api'); // set global prefix
   app.enableCors();
   await app.listen(3000);
 }
