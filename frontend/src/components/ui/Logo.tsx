1eae26dca2fd3071e6903e234669e4fab53d09f4
qinscode <fudong.qin@outlook.com>
2024-08-30 20:09:11 +0800

    style(Logo): add className prop for enhanced reusability
    Enable the Logo component to accept a className prop, allowing consumers to
    apply custom styling. This enhancement makes the Logo component more versatile
    and reusable within different contexts of the application.
    
    style(login): customize Button component with indigo color
    
    Specify a custom background color (indigo-600) for the Button component withinthe login page to align with the updated design requirements. This change
    ensures the login button visually stands out and maintains brand consistency.

--- frontend/src/components/ui/Logo.tsx
+++ frontend/src/components/ui/Logo.tsx
@@ -1,5 +1,4 @@
 import Image from 'next/image'
-
-export function Logo() {
+export function Logo(props: React.ComponentPropsWithoutRef<'svg'>) {
   return <Image src="/uwa-logo.svg" alt="Logo" width={180} height={59} />
 }
