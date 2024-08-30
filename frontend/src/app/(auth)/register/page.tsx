ea936fb2c37b0b4676eff3cd4a31dd033985b2a4
qinscode <fudong.qin@outlook.com>
2024-08-30 19:25:25 +0800

    refactor(register): update page title to 'Register'
    Change the registration page's title from 'Get started for free' to 'Register' for
    clarity and to better reflect the page's purpose. This concise title aligns with user
    expectations and improves the overall user experience.

--- frontend/src/app/(auth)/register/page.tsx
+++ frontend/src/app/(auth)/register/page.tsx
@@ -18,7 +18,7 @@ export default function Register() {
           <Logo className="h-10 w-auto" />
         </Link>
       </div>
-      <h2 className="mt-20 text-lg font-semibold text-gray-900">Get started for free</h2>
+      <h2 className="mt-20 text-lg font-semibold text-gray-900">Register</h2>
       <p className="mt-2 text-sm text-gray-700">
         Already registered?{' '}
         <Link href={'/login'} className="font-medium text-blue-600 hover:underline">
