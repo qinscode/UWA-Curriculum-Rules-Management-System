534d354cbd153126a10bb5a72e1e3fded30e24f6
qinscode <fudong.qin@outlook.com>
2024-08-30 17:57:32 +0800

    refactor(page): remove initial implementation of SortableTree
    The SortableTree component along with its requirements data and related imports have been
    removed from the page.tsx. This change is preparing the structure for a new implementation
    or adjustments following up the initial setup.

--- frontend/src/app/test-component/page.tsx
+++ frontend/src/app/test-component/page.tsx
@@ -1,33 +1,12 @@
 'use client'
 import React from 'react'
-import dynamic from 'next/dynamic'
-import SortedTree from '@/components/manage-rules/common/SortedTree'
-import { initialViableRequirementData } from './initialData'
-import { Button } from '@/components/ui/button'
-import { Requirement } from '@/types'
 
 // Dynamically import the SortableTreeComponent to avoid SSR issues
 
 const SortableTreePage: React.FC = () => {
-  const [useRequirement, setUseRequirement] = React.useState(initialViableRequirementData)
   return (
     <div className="container mx-auto p-4">
-      <main>
-        <h1 className="mb-4 text-3xl font-bold">Sortable Tree Demo</h1>
-        <Button
-          className="mb-3"
-          onClick={() => {
-            console.log(useRequirement)
-          }}
-        >
-          Print
-        </Button>
-        <SortedTree
-          initialData={useRequirement}
-          onUpdateRequirement={setUseRequirement}
-          presetRequirements={initialViableRequirementData}
-        />
-      </main>
+      <main></main>
     </div>
   )
 }
