a073e038924a52e1fddcf04b2f3f6e361c251ba6
qinscode <fudong.qin@outlook.com>
2024-09-08 19:43:56 +0800

    feat(rules): introduce rule type enum
    Adds a `RuleType` enum to categorize rules and
    updates the `Rule` entity to use it. This improves
    organization and clarity within the rules
    system.

--- backend/src/migrations/1725794828818-UpdateEmue.ts
+++ backend/src/migrations/1725794828818-UpdateEmue.ts
@@ -1,18 +1,23 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm'
 
 export class UpdateEmue1725794828818 implements MigrationInterface {
-    name = 'UpdateEmue1725794828818'
+  name = 'UpdateEmue1725794828818'
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`rules\` DROP COLUMN \`type\``);
-        await queryRunner.query(`ALTER TABLE \`rules\` ADD \`type\` enum ('English language eligibility requirements', 'Admissions', 'Satisfactory Progress', 'Progress Status', 'Award with distinction', 'Deferrals', 'Additional rules', 'Outcomes & Australian Qualifications Framework', 'Skills', 'Application of knowledge to skills') NOT NULL`);
-        await queryRunner.query(`ALTER TABLE \`courses\` CHANGE \`type\` \`type\` enum ('Graduate Certificate', 'Graduate Diploma', 'Master''s (Coursework)', 'Master''s Extended', 'Master''s Research', 'Doctoral Degree', 'Professional Doctorate/Master''s Coursework') NOT NULL`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE \`rules\` DROP COLUMN \`type\``)
+    await queryRunner.query(
+      `ALTER TABLE \`rules\` ADD \`type\` enum ('English language eligibility requirements', 'Admissions', 'Satisfactory Progress', 'Progress Status', 'Award with distinction', 'Deferrals', 'Additional rules', 'Outcomes & Australian Qualifications Framework', 'Skills', 'Application of knowledge to skills') NOT NULL`
+    )
+    await queryRunner.query(
+      `ALTER TABLE \`courses\` CHANGE \`type\` \`type\` enum ('Graduate Certificate', 'Graduate Diploma', 'Master''s (Coursework)', 'Master''s Extended', 'Master''s Research', 'Doctoral Degree', 'Professional Doctorate/Master''s Coursework') NOT NULL`
+    )
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`courses\` CHANGE \`type\` \`type\` enum ('Graduate Certificate', 'Graduate Diploma', 'Master''''s (Coursework)', 'Master''''s Extended', 'Master''''s Research', 'Doctoral Degree', 'Professional Doctorate/Master''''s Coursework') NOT NULL`);
-        await queryRunner.query(`ALTER TABLE \`rules\` DROP COLUMN \`type\``);
-        await queryRunner.query(`ALTER TABLE \`rules\` ADD \`type\` varchar(255) NOT NULL`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE \`courses\` CHANGE \`type\` \`type\` enum ('Graduate Certificate', 'Graduate Diploma', 'Master''''s (Coursework)', 'Master''''s Extended', 'Master''''s Research', 'Doctoral Degree', 'Professional Doctorate/Master''''s Coursework') NOT NULL`
+    )
+    await queryRunner.query(`ALTER TABLE \`rules\` DROP COLUMN \`type\``)
+    await queryRunner.query(`ALTER TABLE \`rules\` ADD \`type\` varchar(255) NOT NULL`)
+  }
 }
