4f6fd3575cecec319cbbda2a42f3919e77cd8274
qinscode <fudong.qin@outlook.com>
2024-09-09 22:51:22 +0800

    feat(courses): add 'Eger' course type
    Alter the 'type' column in the 'courses' table to include 'Eger' as an additional option in
    the enumeration list. This allows for the new course type to be managed within the existing
    database structure.

---
+++ backend/src/migrations/1725891880960-AddEger.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class AddEger1725891880960 implements MigrationInterface {
+    name = 'AddEger1725891880960'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE \`courses\` CHANGE \`type\` \`type\` enum ('Graduate Certificate', 'Graduate Diploma', 'Master''s (Coursework)', 'Master''s Extended', 'Master''s Research', 'Doctoral Degree', 'Professional Doctorate/Master''s Coursework') NOT NULL`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE \`courses\` CHANGE \`type\` \`type\` enum ('Graduate Certificate', 'Graduate Diploma', 'Master''''s (Coursework)', 'Master''''s Extended', 'Master''''s Research', 'Doctoral Degree', 'Professional Doctorate/Master''''s Coursework') NOT NULL`);
+    }
+
+}
