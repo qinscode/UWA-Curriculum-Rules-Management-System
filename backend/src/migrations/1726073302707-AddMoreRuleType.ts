713e59ad3fa311c34a701b9c35fa3d2ee7d728ec
qinscode <fudong.qin@outlook.com>
2024-09-12 00:58:53 +0800

    feat(rules): add more rule types and fix course type enumeration
    - Added new rule types including 'Applicability of the Student Rules, policies and procedures',
      'Academic Conduct Essentials and Communication and Research Skills modules',
      'ARTICULATION_AND_EXIT_AWARDS', and 'COURSE_STRUCTURE'.
    - Adjusted the course type enumeration to correct the possessive form of "Master's".- Fixed an issue with the 'type' column in the 'rules' table by consolidating the enumeration
      list and correcting typos.

---
+++ backend/src/migrations/1726073302707-AddMoreRuleType.ts
@@ -0,0 +1,22 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class AddMoreRuleType1726073302707 implements MigrationInterface {
+    name = 'AddMoreRuleType1726073302707'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE \`preset_requirements\` CHANGE \`style\` \`style\` varchar(255) NOT NULL DEFAULT 'aa.ts'`);
+        await queryRunner.query(`ALTER TABLE \`courses\` CHANGE \`type\` \`type\` enum ('Graduate Certificate', 'Graduate Diploma', 'Master''s (Coursework)', 'Master''s Extended', 'Master''s Research', 'Doctoral Degree', 'Professional Doctorate/Master''s Coursework') NOT NULL`);
+        await queryRunner.query(`ALTER TABLE \`rules\` CHANGE \`type\` \`type\` enum ('Applicability of the Student Rules, policies and procedures', 'Academic Conduct Essentials and Communication and Research Skills modules', 'English Eligibility', 'Admissions', 'Satisfactory Progress', 'Progress Status', 'Award with Distinction', 'Deferrals', 'Additional Rules', 'Skills', 'Knowledge Application', 'Knowledge', 'Ranking and selection for admission', 'ARTICULATION_AND_EXIT_AWARDS', 'COURSE_STRUCTURE') NOT NULL`);
+        await queryRunner.query(`ALTER TABLE \`requirements\` DROP COLUMN \`style\``);
+        await queryRunner.query(`ALTER TABLE \`requirements\` ADD \`style\` enum ('numeric', 'alphabetic', 'roman', 'none') NOT NULL DEFAULT 'numeric'`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE \`requirements\` DROP COLUMN \`style\``);
+        await queryRunner.query(`ALTER TABLE \`requirements\` ADD \`style\` varchar(255) NOT NULL DEFAULT 'aa'`);
+        await queryRunner.query(`ALTER TABLE \`rules\` CHANGE \`type\` \`type\` enum ('English Eligibility', 'Admissions', 'Satisfactory Progress', 'Progress Status', 'Award with Distinction', 'Deferrals', 'Additional Rules', 'Skills', 'Knowledge Application', 'Knowledge', 'Ranking and selection for admission') NOT NULL`);
+        await queryRunner.query(`ALTER TABLE \`courses\` CHANGE \`type\` \`type\` enum ('Graduate Certificate', 'Graduate Diploma', 'Master''''s (Coursework)', 'Master''''s Extended', 'Master''''s Research', 'Doctoral Degree', 'Professional Doctorate/Master''''s Coursework') NOT NULL`);
+        await queryRunner.query(`ALTER TABLE \`preset_requirements\` CHANGE \`style\` \`style\` varchar(255) NOT NULL DEFAULT 'aa'`);
+    }
+
+}
---
+++ backend/src/migrations/1726073581738-BugFix.ts
@@ -0,0 +1,16 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class BugFix1726073581738 implements MigrationInterface {
+    name = 'BugFix1726073581738'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE \`courses\` CHANGE \`type\` \`type\` enum ('Graduate Certificate', 'Graduate Diploma', 'Master''s (Coursework)', 'Master''s Extended', 'Master''s Research', 'Doctoral Degree', 'Professional Doctorate/Master''s Coursework') NOT NULL`);
+        await queryRunner.query(`ALTER TABLE \`rules\` CHANGE \`type\` \`type\` enum ('Applicability of the Student Rules policies and procedures', 'Academic Conduct Essentials and Communication and Research Skills modules', 'English Eligibility', 'Admissions', 'Satisfactory Progress', 'Progress Status', 'Award with Distinction', 'Deferrals', 'Additional Rules', 'Skills', 'Knowledge Application', 'Knowledge', 'Ranking and selection for admission', 'ARTICULATION_AND_EXIT_AWARDS', 'COURSE_STRUCTURE') NOT NULL`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE \`rules\` CHANGE \`type\` \`type\` enum ('Applicability of the Student Rule', 'policies and procedures', 'Academic Conduct Essentials and Communication and Research Skills modules', 'English Eligibility', 'Admissions', 'Satisfactory Progress', 'Progress Status', 'Award with Distinction', 'Deferrals', 'Additional Rules', 'Skills', 'Knowledge Application', 'Knowledge', 'Ranking and selection for admission', 'ARTICULATION_AND_EXIT_AWARDS', 'COURSE_STRUCTURE') NOT NULL`);
+        await queryRunner.query(`ALTER TABLE \`courses\` CHANGE \`type\` \`type\` enum ('Graduate Certificate', 'Graduate Diploma', 'Master''''s (Coursework)', 'Master''''s Extended', 'Master''''s Research', 'Doctoral Degree', 'Professional Doctorate/Master''''s Coursework') NOT NULL`);
+    }
+
+}
