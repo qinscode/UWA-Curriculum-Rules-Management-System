7e58299172b3bd4d0a1ddb9dfbbf9fb41f307452
Ella Zhang <23844446@student.uwa.edu.au>
2024-09-18 20:13:15 +0800

    Revert "Change the bg from gray to blue to better match up the theme"
    This reverts commit e311a224972d81e6c2c82290b2ca15bb5cb4d744.

--- frontend/src/components/manage-rules/common/RuleSection.tsx
+++ frontend/src/components/manage-rules/common/RuleSection.tsx
@@ -9,8 +9,8 @@ interface RuleSectionProps {
 const RuleSection: React.FC<RuleSectionProps> = ({ title, children }) => (
   <section className="mb-8">
     <h3 className="mb-4 text-lg font-medium leading-6 text-gray-900">{title}</h3>
-    <Card className="shadow-lg">
-      <CardContent className="bg-blue-50 p-6">{children}</CardContent>
+    <Card className="shadow-md">
+      <CardContent className="bg-gray-50 p-6">{children}</CardContent>
     </Card>
   </section>
 )
