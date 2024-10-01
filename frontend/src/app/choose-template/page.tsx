5b03993788e6e370870c46758635290b8cdb9637
eggplant0-0 <maoxuechen126@126.com>
2024-10-01 22:42:37 +0800

    add page.tsx

--- frontend/src/app/choose-template/page.tsx
+++ frontend/src/app/choose-template/page.tsx
@@ -4,14 +4,15 @@ import React from 'react'
 import CourseManage from '@/components/manage-course/ManageCourse'
 import Layout from '@/components/Layout'
 import withAuth from '@/components/auth/withAuth'
+import TemplateManagementPage from "@/components/choose-template/ChooseTemplate";
 
-function CourseManagePage() {
+function ChooseTemplatePage() {
   return (
     <Layout>
-      <CourseManage />
+      <TemplateManagementPage />
     </Layout>
   )
 }
 
 // 使用 withAuth 包裹组件
-export default withAuth(CourseManagePage)
+export default withAuth(ChooseTemplatePage)
