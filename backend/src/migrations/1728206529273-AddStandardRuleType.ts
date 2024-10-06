bd46bbfc31ebdf6967a5df1283f2da768fe514cc
qinscode <23992836@student.uwa.edu.au>
2024-10-06 18:41:48 +0800

    fix(database): standardize course type enum across tables
    Ensure consistent enum options for course types across preset_requirements, courses, and preset_rules tables by combining and correcting the enum values in each table. This update improves database integrity and removes previous inconsistencies in enum definitions.

---
+++ backend/src/migrations/1728206529273-AddStandardRuleType.ts
@@ -0,0 +1,16 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class AddStandardRuleType1728206529273 implements MigrationInterface {
+    name = 'AddStandardRuleType1728206529273'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE \`courses\` CHANGE \`type\` \`type\` enum ('Graduate Certificate', 'Graduate Diploma', 'Master''s (Coursework)', 'Master''s Extended', 'Master''s Research', 'Doctoral Degree', 'Professional Doctorate/Master''s Coursework') NOT NULL`);
+        await queryRunner.query(`ALTER TABLE \`preset_rules\` CHANGE \`course_type\` \`course_type\` enum ('Graduate Certificate', 'Graduate Diploma', 'Master''s (Coursework)', 'Master''s Extended', 'Master''s Research', 'Doctoral Degree', 'Professional Doctorate/Master''s Coursework') NOT NULL`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE \`preset_rules\` CHANGE \`course_type\` \`course_type\` enum ('Graduate Certificate', 'Graduate Diploma', 'Master''''s (Coursework)', 'Master''''s Extended', 'Master''''s Research', 'Doctoral Degree', 'Professional Doctorate/Master''''s Coursework') NOT NULL`);
+        await queryRunner.query(`ALTER TABLE \`courses\` CHANGE \`type\` \`type\` enum ('Graduate Certificate', 'Graduate Diploma', 'Master''''s (Coursework)', 'Master''''s Extended', 'Master''''s Research', 'Doctoral Degree', 'Professional Doctorate/Master''''s Coursework') NOT NULL`);
+    }
+
+}
