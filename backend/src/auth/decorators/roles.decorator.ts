95318a59a46c33778d24158c279524d2568d09b7
qinscode <23992836@student.uwa.edu.au>
2024-10-07 18:41:32 +0800

    refactor(auth): change ROLES_KEY from 'roles' to 'role' and make user role readonly
    - Update ROLES_KEY constant name for consistency- Modify user entity to make role property readonly for better practice

--- backend/src/auth/decorators/roles.decorator.ts
+++ backend/src/auth/decorators/roles.decorator.ts
@@ -1,5 +1,5 @@
 import { SetMetadata } from '@nestjs/common'
 import { UserType } from '../../users/entities/user.enum'
 
-export const ROLES_KEY = 'roles'
+export const ROLES_KEY = 'role'
 export const Roles = (...roles: UserType[]) => SetMetadata(ROLES_KEY, roles)
