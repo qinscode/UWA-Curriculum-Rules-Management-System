8b0decdb85288084602bd01125b22aa75d415e17
qinscode <23992836@student.uwa.edu.au>
2024-10-08 22:20:10 +0800

    fix(migrations): correct course type enum values
    Remove unnecessary single quotes from enum values for course types in both `courses` and `preset-courses` tables. This change improves data consistency and avoids potential issues with query parsing.

---
+++ backend/src/migrations/1728376180209-AddReadOnly.ts
@@ -0,0 +1,20 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class AddReadOnly1728376180209 implements MigrationInterface {
+    name = 'AddReadOnly1728376180209'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE \`courses\` CHANGE \`type\` \`type\` enum ('Graduate Certificate', 'Graduate Diploma', 'Master''s (Coursework)', 'Master''s Extended', 'Master''s Research', 'Doctoral Degree', 'Professional Doctorate/Master''s Coursework') NOT NULL`);
+        await queryRunner.query(`DROP INDEX \`IDX_0023f81631f4d9ab057ad0b487\` ON \`preset-courses\``);
+        await queryRunner.query(`ALTER TABLE \`preset-courses\` CHANGE \`type\` \`type\` enum ('Graduate Certificate', 'Graduate Diploma', 'Master''s (Coursework)', 'Master''s Extended', 'Master''s Research', 'Doctoral Degree', 'Professional Doctorate/Master''s Coursework') NOT NULL`);
+        await queryRunner.query(`CREATE UNIQUE INDEX \`IDX_0023f81631f4d9ab057ad0b487\` ON \`preset-courses\` (\`code\`, \`type\`)`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP INDEX \`IDX_0023f81631f4d9ab057ad0b487\` ON \`preset-courses\``);
+        await queryRunner.query(`ALTER TABLE \`preset-courses\` CHANGE \`type\` \`type\` enum ('Graduate Certificate', 'Graduate Diploma', 'Master''''s (Coursework)', 'Master''''s Extended', 'Master''''s Research', 'Doctoral Degree', 'Professional Doctorate/Master''''s Coursework') NOT NULL`);
+        await queryRunner.query(`CREATE UNIQUE INDEX \`IDX_0023f81631f4d9ab057ad0b487\` ON \`preset-courses\` (\`code\`, \`type\`)`);
+        await queryRunner.query(`ALTER TABLE \`courses\` CHANGE \`type\` \`type\` enum ('Graduate Certificate', 'Graduate Diploma', 'Master''''s (Coursework)', 'Master''''s Extended', 'Master''''s Research', 'Doctoral Degree', 'Professional Doctorate/Master''''s Coursework') NOT NULL`);
+    }
+
+}
