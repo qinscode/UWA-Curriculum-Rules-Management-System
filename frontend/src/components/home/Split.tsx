3f0644f748acf5cec7867e898682d8233d8e333b
Ella Zhang <23844446@student.uwa.edu.au>
2024-10-14 17:53:19 +0800

    Add system Overview and description, link the get started button to login page and learn more to manage course page

--- frontend/src/components/home/Split.tsx
+++ frontend/src/components/home/Split.tsx
@@ -62,12 +62,12 @@ const Split: FC = () => {
           </p>
           <div className="mt-10 flex items-center gap-x-6">
             <a
-              href="/manage-course"
+              href="/login"
               className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
               Get started
             </a>
-            <a href="/frontend/public" className="text-sm font-semibold leading-6 text-gray-900">
+            <a href="/manage-course" className="text-sm font-semibold leading-6 text-gray-900">
               Learn more <span aria-hidden="true">→</span>
             </a>
           </div>
