4338b3e1ec991c236e4b8dc4c6ef51818689a5a1
qinscode <23992836@student.uwa.edu.au>
2024-10-15 03:03:10 +0800

    refactor(rules): reorganize RuleType enum for better categorization
    Reorder the RuleType enum to group similar rules together for improved readability and maintainability. This reorganization does not change the existing functionality but enhances the structure of the code by placing related rules in a more logical sequence.

--- backend/src/rules/entities/rule.enum.ts
+++ backend/src/rules/entities/rule.enum.ts
@@ -1,13 +1,20 @@
 export enum RuleType {
   ASR = 'Applicability of the Student Rules policies and procedures',
   ACECRS = 'Academic Conduct Essentials and Communication and Research Skills modules',
+
   ENGLISH_ELIGIBILITY = 'English Eligibility',
   ADMISSIONS = 'Admissions',
-  SATISFACTORY_PROGRESS = 'Satisfactory Progress',
-  PROGRESS_STATUS = 'Progress Status',
-  AWARD_WITH_DISTINCTION = 'Award with Distinction',
-  DEFERRALS = 'Deferrals',
   RANKING_AND_SELECTION = 'Ranking and selection for admission',
+
   ARTICULATION_EXIT_AWARD = 'Articulation Exit Award',
+
   COURSE_STRUCTURE = 'Course Structure',
+
+  SATISFACTORY_PROGRESS = 'Satisfactory Progress',
+
+  PROGRESS_STATUS = 'Progress Status',
+
+  AWARD_WITH_DISTINCTION = 'Award with Distinction',
+
+  DEFERRALS = 'Deferrals',
 }
