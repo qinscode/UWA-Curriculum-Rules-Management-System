1f3435f064d648991ca7f48f10c84bf72454af99
Ella Zhang <23844446@student.uwa.edu.au>
2024-10-14 17:38:28 +0800

    Change the header text

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -7,11 +7,13 @@ const Example: FC = () => {
       <div className="mx-auto max-w-2xl text-center">
         <p className="text-base font-semibold leading-7 text-indigo-600">Welcome to</p>
         <h2 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-          Course Rules Management System (Demo)
+          UWA Curriculum Management System
         </h2>
         <p className="mt-6 text-lg leading-8 text-gray-600">
-          This centralized platform allows you to manage and update course rules efficiently. Use
-          the navigation menu to access different features.
+          A centralized platform designed with a user-friendly interface to manage course rules
+          across various university programs at UWA. Integrating both standard and custom criteria,
+          ensuring compliance with the university's governance requirements for formatting and
+          documentation.
         </p>
       </div>
       {/*<div className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-6 sm:mt-20 md:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-4 lg:gap-8">*/}
