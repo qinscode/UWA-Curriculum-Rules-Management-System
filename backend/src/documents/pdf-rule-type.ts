7e3a46aba510aa9c219c5587bb148bdfe3d90777
qinscode <23992836@student.uwa.edu.au>
2024-10-14 17:18:44 +0800

    refactor(pdf-rule-type): reorganize and update rule type mappings
    - Reorder the mapping object for better logical grouping of rule types
    - Update mappings for new rule types (ASR, ACECRS, COURSE_STRUCTURE)- Remove obsolete rule type mappings (ADDITIONAL_RULES, SKILLS, KNOWLEDGE_APPLICATION, KNOWLEDGE)
    - Improve readability by adding line breaks between different rule type mappings

--- backend/src/documents/pdf-rule-type.ts
+++ backend/src/documents/pdf-rule-type.ts
@@ -2,22 +2,26 @@ import { RuleType } from '../rules/entities/rule.enum'
 
 export class PDFRuleType {
   static readonly mapping: {
-    [RuleType.RANKING_AND_SELECTION]: string
-    [RuleType.ARTICULATION_EXIT_AWARD]: string
-    [RuleType.ENGLISH_ELIGIBILITY]: string
-    [RuleType.KNOWLEDGE_APPLICATION]: string
-    [RuleType.SATISFACTORY_PROGRESS]: string
-    [RuleType.PROGRESS_STATUS]: string
-    [RuleType.ADDITIONAL_RULES]: string
-    [RuleType.KNOWLEDGE]: string
-    [RuleType.AWARD_WITH_DISTINCTION]: string
-    [RuleType.SKILLS]: string
-    [RuleType.ADMISSIONS]: string
-    [RuleType.DEFERRALS]: string
-    [RuleType.COURSE_STRUCTURE]: string
     [RuleType.ASR]: string
     [RuleType.ACECRS]: string
+    [RuleType.ENGLISH_ELIGIBILITY]: string
+    [RuleType.ADMISSIONS]: string
+    [RuleType.RANKING_AND_SELECTION]: string
+
+    [RuleType.ARTICULATION_EXIT_AWARD]: string
+
+    [RuleType.COURSE_STRUCTURE]: string
+
+    [RuleType.SATISFACTORY_PROGRESS]: string
+
+    [RuleType.PROGRESS_STATUS]: string
+
+    [RuleType.AWARD_WITH_DISTINCTION]: string
+
+    [RuleType.DEFERRALS]: string
   } = {
+    [RuleType.ASR]: 'Applicability of the Student Rules policies and procedures',
+    [RuleType.ACECRS]: 'Academic Conduct Essentials and Communication and Research Skills modules',
     // Admission rules
     [RuleType.ENGLISH_ELIGIBILITY]: 'Admission rules - English language competency requirements',
     [RuleType.ADMISSIONS]: 'Admission rules - admission requirements',
@@ -35,12 +39,6 @@ export class PDFRuleType {
     [RuleType.PROGRESS_STATUS]: 'Progress Status ',
     [RuleType.AWARD_WITH_DISTINCTION]: 'Award with Distinction Rules',
     [RuleType.DEFERRALS]: 'Deferral Rules',
-    [RuleType.ADDITIONAL_RULES]: 'Additional Rules',
-    [RuleType.SKILLS]: 'Skills Rules',
-    [RuleType.KNOWLEDGE_APPLICATION]: 'Knowledge Application Rules',
-    [RuleType.KNOWLEDGE]: 'Knowledge Rules',
-    [RuleType.ASR]: 'Applicability of the Student Rules policies and procedures',
-    [RuleType.ACECRS]: 'Academic Conduct Essentials and Communication and Research Skills modules',
   }
 
   static getPrintRuleType(ruleType: RuleType): string {
